c2d63406bbce83be6c4443a025f43aaf91afd515
Mikolaj Wojtysiak-Wawrzyniak <mikolaj.w.w17@gmail.com>
2019-05-24 13:37:48 +0200

    Added simple SwipeRefreshLayout

--- app/src/main/java/ife/cs/weatherappdt/MainActivity.kt
+++ app/src/main/java/ife/cs/weatherappdt/MainActivity.kt
@@ -2,11 +2,18 @@ package ife.cs.weatherappdt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        swiperefresh.setOnRefreshListener {
+            textView.setText("${++counter}")
+            swiperefresh.setRefreshing(false)
+        }
     }
 }
