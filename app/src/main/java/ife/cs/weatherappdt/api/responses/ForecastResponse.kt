2cc6669de89ae895e73c435984c59ca523a3c029
Mikołaj Wojtysiak-Wawrzyniak <mikolaj.w.w17@gmail.com>
2019-06-08 17:01:38 +0200

    Added default null value to response fields

--- app/src/main/java/ife/cs/weatherappdt/api/responses/ForecastResponse.kt
+++ app/src/main/java/ife/cs/weatherappdt/api/responses/ForecastResponse.kt
@@ -3,57 +3,57 @@ package ife.cs.weatherappdt.api.responses
 import com.beust.klaxon.Json
 
 data class ForecastResponse(
-    val city: City?,
-    val cnt: Int?, // 40
-    val cod: String?, // 200
+    val city: City? = null,
+    val cnt: Int? = null, // 40
+    val cod: String? = null, // 200
     val list: List<X>,
-    val message: Double? // 0.0074
+    val message: Double? = null // 0.0074
 ) {
     data class City(
-        val coord: Coord?,
-        val country: String?, // PL
-        val id: Int?, // 3093133
-        val name: String?, // Lodz
-        val population: Int?, // 768755
-        val timezone: Int? // 7200
+        val coord: Coord? = null,
+        val country: String? = null, // PL
+        val id: Int? = null, // 3093133
+        val name: String? = null, // Lodz
+        val population: Int? = null, // 768755
+        val timezone: Int? = null // 7200
     ) {
         data class Coord(
-            val lat: Double?, // 51.75
-            val lon: Double? // 19.4667
+            val lat: Double? = null, // 51.75
+            val lon: Double? = null // 19.4667
         )
     }
 
     data class X(
-        val clouds: Clouds?,
-        val dt: Long?, // 1559314800
-        val dt_txt: String?, // 2019-05-31 15:00:00
-        val main: Main?,
+        val clouds: Clouds? = null,
+        val dt: Long? = null, // 1559314800
+        val dt_txt: String? = null, // 2019-05-31 15:00:00
+        val main: Main? = null,
         val rain: Rain? = null,
         val snow: Snow? = null,
-        val sys: Sys?,
-        val weather: List<Weather?>?,
-        val wind: Wind?
+        val sys: Sys? = null,
+        val weather: List<Weather?>? = null,
+        val wind: Wind? = null
     ) {
         data class Main(
-            val grnd_level: Double?, // 1007.28
-            val humidity: Double?, // 91
-            val pressure: Double?, // 1019.86
-            val sea_level: Double?, // 1019.86
-            val temp: Double?, // 282.7
-            val temp_kf: Double?, // 0
-            val temp_max: Double?, // 282.7
-            val temp_min: Double? // 282.7
+            val grnd_level: Double? = null, // 1007.28
+            val humidity: Double? = null, // 91
+            val pressure: Double? = null, // 1019.86
+            val sea_level: Double? = null, // 1019.86
+            val temp: Double? = null, // 282.7
+            val temp_kf: Double? = null, // 0
+            val temp_max: Double? = null, // 282.7
+            val temp_min: Double? = null // 282.7
         )
 
         data class Weather(
-            val description: String?, // light rain
-            val icon: String?, // 10d
-            val id: Double?, // 500
-            val main: String? // Rain
+            val description: String? = null, // light rain
+            val icon: String? = null, // 10d
+            val id: Double? = null, // 500
+            val main: String? = null // Rain
         )
 
         data class Clouds(
-            val all: Double? // 100
+            val all: Double? = null // 100
         )
 
         data class Rain(
@@ -62,12 +62,12 @@ data class ForecastResponse(
         )
 
         data class Sys(
-            val pod: String? // d
+            val pod: String? = null // d
         )
 
         data class Wind(
-            val deg: Double?, // 345.657
-            val speed: Double? // 7.38
+            val deg: Double? = null, // 345.657
+            val speed: Double? = null // 7.38
         )
 
         data class Snow(
--- app/src/main/java/ife/cs/weatherappdt/api/responses/WeatherResponse.kt
+++ app/src/main/java/ife/cs/weatherappdt/api/responses/WeatherResponse.kt
@@ -1,57 +1,69 @@
 package ife.cs.weatherappdt.api.responses
 
+import com.beust.klaxon.Json
+
 data class WeatherResponse(
-    val base: String?, // stations
-    val clouds: Clouds?,
-    val cod: Int?, // 200
-    val coord: Coord?,
-    val dt: Int?, // 1558891432
-    val id: Int?, // 3093133
-    val main: Main?,
-    val name: String?, // Lodz
-    val sys: Sys?,
-    val timezone: Int?, // 7200
-    val visibility: Int?, // 10000
-    val weather: List<Weather?>?,
+    val base: String? = null, // stations
+    val clouds: Clouds? = null,
+    val cod: Int? = null, // 200
+    val coord: Coord? = null,
+    val dt: Int? = null, // 1558891432
+    val id: Int? = null, // 3093133
+    val main: Main? = null,
+    val name: String? = null, // Lodz
+    val sys: Sys? = null,
+    val timezone: Int? = null, // 7200
+    val visibility: Int? = null, // 10000
+    val weather: List<Weather?>? = null,
     val wind: Wind? = null,
-    val rain: ForecastResponse.X.Rain? = null,
-    val snow: ForecastResponse.X.Snow? = null
+    val rain: Rain? = null,
+    val snow: Snow? = null
 ) {
     data class Wind(
-        val deg: Int? = 0, // 240
-        val speed: Double? // 5.1
+        val deg: Double? = null, // 240
+        val speed: Double? = null // 5.1
     )
 
     data class Clouds(
-        val all: Int? // 0
+        val all: Int? = null // 0
     )
 
     data class Sys(
-        val country: String?, // PL
-        val id: Int?, // 1706
-        val message: Double?, // 0.0067
-        val sunrise: Int?, // 1558838138
-        val sunset: Int?, // 1558896176
-        val type: Int? // 1
+        val country: String? = null, // PL
+        val id: Int? = null, // 1706
+        val message: Double? = null, // 0.0067
+        val sunrise: Int? = null, // 1558838138
+        val sunset: Int? = null, // 1558896176
+        val type: Int? = null // 1
     )
 
     data class Coord(
-        val lat: Double?, // 51.75
-        val lon: Double? // 19.47
+        val lat: Double? = null, // 51.75
+        val lon: Double? = null // 19.47
     )
 
     data class Main(
-        val humidity: Int?, // 45
-        val pressure: Int?, // 1011
-        val temp: Double?, // 293.47
-        val temp_max: Double?, // 294.26
-        val temp_min: Double? // 292.59
+        val humidity: Int? = null, // 45
+        val pressure: Int? = null, // 1011
+        val temp: Double? = null, // 293.47
+        val temp_max: Double? = null, // 294.26
+        val temp_min: Double? = null // 292.59
     )
 
     data class Weather(
-        val description: String?, // clear sky
-        val icon: String?, // 01d
-        val id: Int?, // 800
-        val main: String? // Clear
+        val description: String? = null, // clear sky
+        val icon: String? = null, // 01d
+        val id: Int? = null, // 800
+        val main: String? = null // Clear
+    )
+
+    data class Rain(
+        @Json(name = "3h")
+        val threeH: Double? = null // 1.812
+    )
+
+    data class Snow(
+        @Json(name = "3h")
+        val threeH: Double? = null
     )
 }
